520568dc10ef22181109e074c36a838e59a3bb63
John Figueiredo <jfigueir33@gmail.com>
2012-09-30 15:47:11 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JohnSecondDatabase::Application.config.secret_token = 'af147fb78425249a6945f99822e79b1041a62d34f443a18782f4c59c43e78d8d2caa88546b5edfe3a24fc70e9660b87773fb5dc719e10501f6a294a562b78416'
