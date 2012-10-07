a38c1973bf76aaf5d0868b2a561b5af3c7210ef8
John Figueiredo <jfigueir33@gmail.com>
2012-10-07 09:29:35 -0700

    Add a full_title helper

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ JohnSecondDatabase::Application.routes.draw do
 
   get "static_pages/about"
 
+  get "static_pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
